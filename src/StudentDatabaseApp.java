df96facb4a973d87073f7f9aaa4d36f114cacfcf
Yury <ryv8@tut.by>
2020-12-06 15:05:07 +0200

    Student's name and year

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,5 +1,8 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        
+        Student student1 = new Student();
+        // Ask how many users we want to add
+
+        // Create n number of new students
     }
 }
